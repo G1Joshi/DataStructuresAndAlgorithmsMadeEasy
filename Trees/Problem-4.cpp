00e25fc74d843dee39d7d47ddd1019e6c37c1a5f
G1Joshi <joshijeevan98@gmail.com>
2021-05-01 17:13:53 +0530

    search element

---
+++ Trees/Problem-4.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int data = 0)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+int search(Node *root, int key)
+{
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+        if (temp->data == key)
+            return 1;
+        if (temp->left)
+            q.push(temp->left);
+        if (temp->right)
+            q.push(temp->right);
+    }
+    return 0;
+}
+
+int main()
+{
+    Node *root = new Node(8);
+    root->left = new Node(2);
+    root->right = new Node(32);
+    root->left->left = new Node(1);
+    root->left->right = new Node(4);
+    root->right->left = new Node(16);
+    root->right->right = new Node(64);
+
+    cout << search(root, 32);
+
+    return 0;
+}
