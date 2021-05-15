5da967152869cad87e6d61cca3400b8f53586dc9
G1Joshi <joshijeevan98@gmail.com>
2021-05-15 08:09:51 +0530

    find size

---
+++ Trees/Problem-6.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int data = 0)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+int size(Node *root)
+{
+    if (root)
+        return size(root->left) + 1 + size(root->right);
+    return 0;
+}
+
+int main()
+{
+    Node *root = new Node(8);
+    root->left = new Node(2);
+    root->right = new Node(32);
+    root->left->left = new Node(1);
+    root->left->right = new Node(4);
+    root->right->left = new Node(16);
+    root->right->right = new Node(64);
+
+    cout << size(root);
+
+    return 0;
+}
